c10c1a4053fe00234b86113356007d04593883af
sejun2 <qpfjf56@gmail.com>
2024-09-01 02:29:06 +0900

    working pokedex list ui

--- app/src/main/java/com/example/todolist/presentation/viewmodel/screen/PokemonListScreen.kt
+++ app/src/main/java/com/example/todolist/presentation/viewmodel/screen/PokemonListScreen.kt
@@ -1,40 +1,30 @@
 package com.example.todolist.presentation.viewmodel.screen
 
-import android.provider.CalendarContract.Colors
-import android.provider.MediaStore
-import android.util.Log
+import android.graphics.Paint.Align
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridCells.Fixed
-import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -47,13 +37,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -65,28 +51,13 @@ import com.example.todolist.R
 import com.example.todolist.domain.model.PokemonSummary
 import com.example.todolist.presentation.viewmodel.PokemonListUiState
 import com.example.todolist.presentation.viewmodel.PokemonSummaryListViewModel
+import com.example.todolist.ui.theme.Primary
 import com.example.todolist.ui.theme.TodolistTheme
 
 @Composable
 fun PokemonListScreen() {
     TodolistTheme {
         PokemonListView()
-    }
-}
-
-@Composable
-fun PokemonListItem(pokemonData: PokemonSummary) {
-    Box(
-        modifier = Modifier
-            .padding(4.dp)
-            .background(color = Color.White)
-            .border(
-                color = Color.Gray,
-                width = 2.dp,
-                shape = RoundedCornerShape(12.dp),
-            )
-    ) {
-        Text(text = "${pokemonData.name}\n${pokemonData.url}")
     }
 }
 
@@ -135,7 +106,8 @@ fun PokemonCard(pokemon: PokemonSummary) {
                     shape = RoundedCornerShape(8.dp),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(64.dp).align(Alignment.BottomCenter)
+                        .height(64.dp)
+                        .align(Alignment.BottomCenter)
                 ) {
                     Box(
                         contentAlignment = Alignment.BottomCenter
@@ -156,6 +128,39 @@ fun PokemonCard(pokemon: PokemonSummary) {
 }
 
 @Composable
+fun PokemonListHeader() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .background(color = Primary)
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+    ) {
+        Image(
+            modifier = Modifier
+                .size(28.dp)
+                .padding(end = 6.dp),
+            painter = painterResource(id = R.drawable.ic_pokedex_logo),
+            contentDescription = "logo icon",
+        )
+        Text(
+            text = "Pokédex",
+            style = MaterialTheme.typography.headlineMedium.copy(color = Color.White),
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PokedexSearchBar() {
+    SearchBar(query = "", onQueryChange = {}, onSearch = {
+
+    }, active = true, onActiveChange = {}) {
+
+    }
+}
+
+@Composable
 fun PokemonListView(pokemonSummaryListViewModel: PokemonSummaryListViewModel = hiltViewModel()) {
     val uiState by pokemonSummaryListViewModel.uiState.collectAsState()
     val lazyGridState = rememberLazyGridState()
@@ -167,27 +172,39 @@ fun PokemonListView(pokemonSummaryListViewModel: PokemonSummaryListViewModel = h
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .background(color = Primary)
     ) {
-        Text(
-            text = "Pokemon List",
-            style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
+        PokemonListHeader()
 
-        when (val state = uiState) {
-            is PokemonListUiState.Loading -> CircularProgressIndicator()
-            is PokemonListUiState.Error -> Text(
-                "Error: ${state.toString()}",
-                color = MaterialTheme.colorScheme.error
-            )
-
-            is PokemonListUiState.Success -> {
-                PokemonList(
-                    pokemonList = state.pokemonList,
-                    lazyGridState = lazyGridState,
-                    onLoadMore = { pokemonSummaryListViewModel.loadMore() }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 6.dp)
+                .background(
+                    color = Color.White, shape = RoundedCornerShape(
+                        topStart = 12.dp, topEnd = 12.dp
+                    )
                 )
+                .clip(shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp))
+        ) {
+            when (val state = uiState) {
+                is PokemonListUiState.Loading -> CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center)
+                )
+
+                is PokemonListUiState.Error -> Text(
+                    "Error: ${state}",
+                    color = MaterialTheme.colorScheme.error,
+                    modifier = Modifier.align(Alignment.Center),
+                    )
+
+                is PokemonListUiState.Success -> {
+                    PokemonList(
+                        pokemonList = state.pokemonList,
+                        lazyGridState = lazyGridState,
+                        onLoadMore = { pokemonSummaryListViewModel.loadMore() }
+                    )
+                }
             }
         }
     }
