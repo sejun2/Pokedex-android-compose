c5cf0bad66d2ce0d22078ce8ab35f7df8e8fbfb9
sejun2 <qpfjf56@gmail.com>
2024-09-04 18:30:03 +0900

    add pokemondetailviewmodel todo

--- app/src/main/java/com/example/pokemons/presentation/viewmodel/PokemonDetailViewModel.kt
+++ app/src/main/java/com/example/pokemons/presentation/viewmodel/PokemonDetailViewModel.kt
@@ -3,5 +3,7 @@ package com.example.pokemons.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 
 class PokemonDetailViewModel: ViewModel() {
+    // pokemon detail model list variable
 
+    // fetch detail model list
 }
