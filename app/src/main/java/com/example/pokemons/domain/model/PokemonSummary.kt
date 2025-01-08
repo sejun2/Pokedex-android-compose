b05860b209c4b3fdb0bbd93ae0b7fb0d9fcc63f5
sejun2 <qpfjf56@gmail.com>
2025-01-08 11:00:43 +0900

    fix model state to immutable

--- app/src/main/java/com/example/pokemons/domain/model/PokemonSummary.kt
+++ app/src/main/java/com/example/pokemons/domain/model/PokemonSummary.kt
@@ -3,6 +3,6 @@ package com.example.pokemons.domain.model
 data class PokemonSummary(
     val name: String,
     val url: String,
-    var index: Int,
+    val index: Int,
     val imageSrc: String = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png"
 )
